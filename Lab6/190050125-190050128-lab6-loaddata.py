65db4c4a6c18250453dfe9e1e7fbd82b1e2bf56f
vibhav011 <vibhav200601@gmail.com>
2022-03-09 12:44:51 +0530

    Python script done

---
+++ Lab6/190050125-190050128-lab6-loaddata.py
@@ -0,0 +1,29 @@
+import csv
+
+def main_function(filename):
+    NODES = [('users','User','name'), ('tweets','Tweet','text'), ('hashtags','Hashtag','tag')]
+    RELS = [('follows', 'Follows', 'User', 'User'), ('sent', 'Sent', 'User', 'Tweet'), ('mentions', 'Mentions', 'Tweet', 'User'), ('contains', 'Contains', 'Tweet', 'Hashtag')]
+
+    query_string = ''
+
+    for k in NODES:
+        with open('lab6-data/' + k[0] + '.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            data = [tuple(map(lambda x : str(x), row)) for row in reader]
+            query = ','.join(list(map(lambda t: f'({k[1]}_{t[0]}:{k[1]}{{{k[2]}: "{t[1]}"}})', data)))
+            query_string += 'CREATE ' + query + '\n'
+    
+    for k in RELS:
+        with open('lab6-data/' + k[0] + '.csv', 'r') as f:
+            reader = csv.reader(f)
+            next(reader)
+            data = [tuple(map(lambda x : str(x), row)) for row in reader]
+            query = ','.join(list(map(lambda t: f'({k[2]}_{t[0]})-[:{k[1]}]->({k[3]}_{t[1]})', data)))
+            query_string += 'CREATE ' + query + '\n'
+    
+    with open(filename, 'w') as f:
+        f.write(query_string)
+
+if __name__ == "__main__":
+    main_function('lab6-data.cypher')
