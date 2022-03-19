fe6bb7574e43e44b84b81bc6669f19d8bf9be209
TULIP16 <tulip@cse.iitb.ac.in>
2022-03-19 14:29:13 +0530

    revised q2 task b

--- Lab7/q2.py
+++ Lab7/q2.py
@@ -2,6 +2,30 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import split as pyspark_split
 import re
 
+def reg_split(x):
+    if not isinstance(x, str):
+        x = str(x)
+    rh = re.findall(r'(.*?)\s(?!-)', x)[0]
+    x = re.sub(r'(.*?)\s(?!-)', '', x, 1)
+    x = x.strip()
+    # print(x)
+    rt = re.findall(r'(\[.*?\])', x)[0]
+    x = re.sub(r'(\[.*?\])', '', x, 1)
+    x = x.strip()
+    # print(x)
+    rm = re.findall(r'"(.*?)"', x)[0]
+    x = re.sub(r'"(.*?)"', '', x, 1)
+    x = x.strip()
+    # print(x)
+    rc = re.findall(r'\s*(.*?)\s+', x)[0]
+    x = re.sub(r'\s*(.*?)\s+', '', x, 1)
+    x = x.strip()
+    # print(x)
+    rl = re.findall(r'\s*(.*?)\s+', x)[0]
+    return [rh, rt, rm, rc, rl]
+
+# print(reg_split('66.249.66.194 - - [22/Jan/2019:03:56:20 +0330] "GET /m/filter/b2,p6 HTTP/1.1" 200 19451 "-" "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" "-"'))
+
 spark = SparkSession \
 .builder \
 .appName("PySpark create RDD example") \
@@ -9,18 +33,14 @@ spark = SparkSession \
 .getOrCreate()  
 print(spark)
   
-df = spark.read.text('./archive/access.log')
+df = spark.read.text('small.log')
 rdd1 = df.rdd
 
-# split_col = pyspark_split(df, " ")
-# df = df.withColumn('Remote Host', split_col.getItem(0))
-# df = df.withColumn('Request Timestamp', split_col.getItem(1))
-# df = df.withColumn('Request Method', split_col.getItem(2))
-# df = df.withColumn('Response Code', split_col.getItem(3))
-# df = df.withColumn('Response Length', split_col.getItem(4))
-print(re.split(r'\s+(?=([^"]*"[^"]*")*[^"]*$)', '99.99.188.195 - - [25/Jan/2019:05:33:08 +0330] "GET /static/images/amp/blog.png HTTP/1.1" 200 3863 "https://www.zanbil.ir/m/article/616/%D8%B9%D9%84%D8%AA-%D8%AE%D9%88%D8%A7%D8%A8-%D8%B1%D9%81%D8%AA%D9%86%D8%8C%DA%AF%D8%B2%DA%AF%D8%B2%D8%8C%D8%A8%DB%8C-%D8%AD%D8%B3%DB%8C-%D9%88-%D9%85%D9%88%D8%B1-%D9%85%D9%88%D8%B1-%D8%B4%D8%AF%D9%86-%D8%A7%D9%86%DA%AF%D8%B4%D8%AA%D8%A7%D9%86-%D8%AF%D8%B3%D8%AA-%D9%88-%D8%AF%D8%B1%D9%85%D8%A7%D9%86-%D8%A2%D9%86" "Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/71.0.3578.89 Mobile/15E148 Safari/605.1" "-"'))
-rdd2 = rdd1.map(lambda x: re.split(r'\s+(?=([^"]*"[^"]*")*[^"]*$)', str(x)))
+rdd2 = rdd1.map(lambda x: reg_split(x.value))
 
+cols = ['Remote Host', 'Request Timestamp', 'Request Method', 'Response Code', 'Response Length']
+df2  = rdd2.toDF(cols)
 
-
+rdd3 = df2.rdd
+print(rdd3.collect())
 
