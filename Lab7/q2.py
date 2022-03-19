c33d5e4bae51a833874d7c5097390eacf35290b4
TULIP16 <tulip@cse.iitb.ac.in>
2022-03-19 15:10:42 +0530

    broken up into functions

--- Lab7/q2.py
+++ Lab7/q2.py
@@ -3,44 +3,56 @@ from pyspark.sql.functions import split as pyspark_split
 import re
 
 def reg_split(x):
-    if not isinstance(x, str):
-        x = str(x)
-    rh = re.findall(r'(.*?)\s(?!-)', x)[0]
-    x = re.sub(r'(.*?)\s(?!-)', '', x, 1)
-    x = x.strip()
-    # print(x)
-    rt = re.findall(r'(\[.*?\])', x)[0]
-    x = re.sub(r'(\[.*?\])', '', x, 1)
-    x = x.strip()
-    # print(x)
-    rm = re.findall(r'"(.*?)"', x)[0]
-    x = re.sub(r'"(.*?)"', '', x, 1)
-    x = x.strip()
-    # print(x)
-    rc = re.findall(r'\s*(.*?)\s+', x)[0]
-    x = re.sub(r'\s*(.*?)\s+', '', x, 1)
-    x = x.strip()
-    # print(x)
-    rl = re.findall(r'\s*(.*?)\s+', x)[0]
-    return [rh, rt, rm, rc, rl]
+    try:
+        if not isinstance(x, str):
+            x = str(x)
+        rh = re.findall(r'(.*?)\s', x)[0]
+        x = re.sub(r'(.*?)\s(?!-)', '', x, 1)
+        x = x.strip()
+        # print(x)
+        rt = re.findall(r'(\[.*?\])', x)[0]
+        x = re.sub(r'(\[.*?\])', '', x, 1)
+        x = x.strip()
+        # print(x)
+        rm = re.findall(r'"(.*?)"', x)[0]
+        x = re.sub(r'"(.*?)"', '', x, 1)
+        x = x.strip()
+        # print(x)
+        rc = re.findall(r'\s*(.*?)\s+', x)[0]
+        x = re.sub(r'\s*(.*?)\s+', '', x, 1)
+        x = x.strip()
+        # print(x)
+        rl = re.findall(r'\s*(.*?)\s+', x)[0]
+        return [rh, rt, rm, rc, rl]
+    except:
+        return [None, None, None, None, None]
 
-# print(reg_split('66.249.66.194 - - [22/Jan/2019:03:56:20 +0330] "GET /m/filter/b2,p6 HTTP/1.1" 200 19451 "-" "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" "-"'))
+def task_a(spark, csv_file):
+    df = spark.read.text(csv_file)
+    rdd = df.rdd
+    return rdd
 
-spark = SparkSession \
-.builder \
-.appName("PySpark create RDD example") \
-.config("spark.some.config.option", "some-value") \
-.getOrCreate()  
-print(spark)
-  
-df = spark.read.text('small.log')
-rdd1 = df.rdd
+def task_b(rdd_a):
+    rdd = rdd_a.map(lambda x: reg_split(x.value))
+    cols = ['Remote Host', 'Request Timestamp', 'Request Method', 'Response Code', 'Response Length']
+    df  = rdd.toDF(cols)
+    rdd = df.rdd
+    return rdd
 
-rdd2 = rdd1.map(lambda x: reg_split(x.value))
+def task_c(rdd_b):
+    return rdd_b
 
-cols = ['Remote Host', 'Request Timestamp', 'Request Method', 'Response Code', 'Response Length']
-df2  = rdd2.toDF(cols)
+if __name__ == "__main__":
+    # print(reg_split('66.249.66.194 - - [22/Jan/2019:03:56:20 +0330] "GET /m/filter/b2,p6 HTTP/1.1" 200 19451 "-" "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" "-"'))
 
-rdd3 = df2.rdd
-print(rdd3.collect())
+    spark = SparkSession \
+    .builder \
+    .appName("PySpark create RDD example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()  
+    
+    csv_file = 'small.log'
+    rdd_a = task_a(spark, csv_file)
+    rdd_b = task_b(rdd_a)
+    print(rdd_b.collect())
 
