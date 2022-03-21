2fc5f8517787e3339646a7d70b7780c6430dfa0b
TULIP16 <tulip@cse.iitb.ac.in>
2022-03-21 19:47:11 +0530

    q1 latest

--- Lab7/q1.py
+++ Lab7/q1.py
@@ -2,17 +2,27 @@ from pyspark.sql import SparkSession
 
 def make_pairs(lst):
     pairs = []
-    for i in range(len(lst)):
-        for j in range(i+1, len(lst)):
-            if lst[i] is not None and lst[j] is not None:
-                if lst[i] < lst[j]:
-                    pairs.append((lst[i], lst[j])) 
-                if lst[i] > lst[j]:
-                    pairs.append((lst[j], lst[i])) 
+    try:
+        for i in range(1, int(lst[0])):
+            for j in range(i+1, int(lst[0])):
+                if lst[i] is not None and lst[j] is not None:
+                    if lst[i] < lst[j]:
+                        pairs.append((lst[i], lst[j])) 
+                    if lst[i] > lst[j]:
+                        pairs.append((lst[j], lst[i])) 
+    except:
+        pass
     return pairs
 
 def toCSVLine(data):
   return ','.join(str(d) for d in data)
+
+def toList(data):
+    arr = []
+    arr.append(data[0][0])
+    arr.append(data[0][1])
+    arr.append(data[1])
+    return arr
 
 def val(x):
     return x[1]
@@ -22,19 +32,16 @@ spark = SparkSession \
 .appName("PySpark create RDD example") \
 .config("spark.some.config.option", "some-value") \
 .getOrCreate()  
-print(spark)
   
-df = spark.read.csv('./archive/groceries.csv')
-rdd1 = df.rdd.flatMap(make_pairs)
-#print(rdd1.keys().collect())
+df = spark.read.csv('./new.csv')
+rdd1 = df.rdd.flatMap(make_pairs) # 1.a
 rdd21 = rdd1.map(lambda x: (x,1))
-#print(rdd21.values().collect())
-rdd2 = rdd21.reduceByKey(lambda x,y: x+y)
+rdd2 = rdd21.reduceByKey(lambda x,y: x+y) # 1.b
 
-lines = rdd2.map(toCSVLine)
-lines.toDF().toPandas().to_csv('./count.csv')
+
+rdd2.map(toList).toDF(['Item1', 'Item2', 'Count']).toPandas().to_csv('./count.csv', index=False)
 top5_lst = rdd2.top(5, key=val)
 
-for pair_counts in top5_lst:
+for pair_counts in top5_lst: # 1.c
     print(pair_counts[0])
 
