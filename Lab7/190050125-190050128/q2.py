63d569ce64c23acb6843bffe548602078754d960
vibhav011 <vibhav200601@gmail.com>
2022-03-22 15:10:24 +0530

    added submission zip

--- Lab7/190050125-190050128/q2.py
+++ Lab7/190050125-190050128/q2.py
@@ -1,8 +1,8 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
-from pyspark.sql.types import IntegerType, StringType
+from pyspark.sql.types import IntegerType
 import matplotlib.pyplot as plt
-import re, os, time
+import re, os
 
 def reg_split(x):
     try:
@@ -11,19 +11,19 @@ def reg_split(x):
         rh = re.findall(r'(.*?)\s', x)[0]
         x = re.sub(r'(.*?)\s(?!-)', '', x, 1)
         x = x.strip()
-        # print(x)
+
         rt = re.findall(r'(\[.*?\])', x)[0]
         x = re.sub(r'(\[.*?\])', '', x, 1)
         x = x.strip()
-        # print(x)
+
         rm = re.findall(r'"(.*?)"', x)[0]
         x = re.sub(r'\"(.*?)\"', '', x, 1)
         x = x.strip()
-        # print(x)
+
         rc = re.findall(r'\s*(.*?)\s+', x)[0]
         x = re.sub(r'\s*(.*?)\s+', '', x, 1)
         x = x.strip()
-        # print(x)
+
         rl = re.findall(r'\s*(.*?)\s+', x)[0]
         return [rh, rt, rm, rc, rl]
     except:
@@ -32,31 +32,24 @@ def reg_split(x):
 def clean(x):
     host_pattern = re.compile("\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}")
     if not host_pattern.match(x[0]):
-        # print("host not matching")
         return 1
     
-    # print(x[1])
     time_pattern = re.compile("\[\d\d/[A-Z a-z][A-Z a-z][A-Z a-z]/\d\d\d\d:\d\d:\d\d:\d\d [\+ \-]\d\d\d\d\]")
-    # print(re.findall("\[\d\d/[A-Z][a-z][a-z]/\d\d\d\d:\d\d:\d\d:\d\d [\+ \-]\d\d\d\d\]",x[1]))
+
     if not time_pattern.match(x[1]):
-        # print("time not matching")
         return 1
     
     method_pattern = re.compile(r"[A-Z]* .*")
-    # print(x[2])
-    # print(re.findall(r"\bGET\b|\bPOST\b|\bPUT\b|\bPATCH\b|\bDELETE\b .*", x[2]))
+
     if not method_pattern.match(x[2]):
-        # print("method not matching")
         return 1
 
     response_pattern = re.compile(r"[1 2 3 4 5]\d\d")
     if not response_pattern.match(x[3]):
-        # print("response not matching")
         return 1
     
     length_pattern = re.compile("\d+")
     if not length_pattern.match(x[4]):
-        #print("length not matching")
         return 1
     
     return 0
@@ -69,7 +62,6 @@ def task_a(spark, csv_file):
 def task_b(rdd_a):
     rdd = rdd_a.map(lambda x: reg_split(x.value))
     cols = ['Remote Host', 'Request Timestamp', 'Request Method', 'Response Code', 'Response Length']
-    # print(rdd.top(5))
     df  = rdd.toDF(cols)
     rdd = df.rdd
     return rdd
@@ -93,10 +85,12 @@ def get_rdd(spark, csv_file):
 def part_a(df):
     with open('task-d/a.txt', 'w') as f:
         f.write("HTTP status analysis:\n")
-        f.write(df.groupBy("Response Code").count().toPandas().sort_values("Response Code").to_string(index=False))
-        
-def part_b(df):
-    df_temp = df.groupBy("Response Code").count().toPandas().sort_values('count', ascending=False).head(5)
+        df_cache_a = df.groupBy("Response Code").count().cache()
+        f.write(df_cache_a.toPandas().sort_values("Response Code").to_string(index=False))
+    return df_cache_a
+
+def part_b(df_cache):
+    df_temp = df_cache.toPandas().sort_values('count', ascending=False).head(5)
     tot = df_temp.agg({'count': 'sum'}).to_dict()['count']
     plt.pie(df_temp["count"]/tot, labels=df_temp["Response Code"], autopct='%.1f')
     plt.savefig('task-d/b.jpg')
@@ -104,12 +98,16 @@ def part_b(df):
 def part_c(df):
     with open('task-d/c.txt', 'w') as f:
         f.write("Frequent Hosts:\n")
-        f.write(df.groupBy("Remote Host").count().toPandas().to_string(index=False))
+        df_cache_c = df.groupBy("Remote Host").count().cache()
+        f.write(df_cache_c.toPandas().to_string(index=False))
+    return df_cache_c
 
-def part_d(df):
+def part_d(df_cache):
     with open('task-d/d.txt', 'w') as f:
         f.write("Unique hosts:\n")
-        f.write(df.agg(countDistinct("Remote Host")).toPandas().to_string(index=False))
+        f.write(str(df_cache.count()))
+        # f.write(df.agg(countDistinct("Remote Host")).toPandas().to_string(index=False))
+
 
 def changeFormat(x):
     month = {"Jan":"01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", "Oct":"10", "Nov":"11", "Dec":"12"}
@@ -118,7 +116,7 @@ def changeFormat(x):
 
 def part_e(df, to_print=True):
     df = df.withColumn("Request Timestamp", df['Request Timestamp'].substr(2, 11)) \
-        .groupBy("Request Timestamp").agg(countDistinct("Remote Host")).toPandas()
+        .groupBy("Request Timestamp").agg(countDistinct("Remote Host")).cache().toPandas()
     
     df.rename(columns = {'Request Timestamp':'day', 'count(Remote Host)':'hosts'}, inplace = True)
     
@@ -129,17 +127,18 @@ def part_e(df, to_print=True):
         with open("task-d/e.txt", "w") as f:
             f.write("Unique hosts per day:\n")
             f.write(df.to_string(index=False))
-    
+
     return df
 
-def part_f(df):
-    df = part_e(df, to_print=False)
-    plot = df.plot(x="day", y="hosts", kind="line", title="No of unique hosts daily", xlabel="Day", ylabel="Hosts Count")
+def part_f(df_cache):
+    # df = part_e(df, to_print=False)
+    plot = df_cache.plot(x="day", y="hosts", kind="line", title="No of unique hosts daily", xlabel="Day", ylabel="Hosts Count")
     fig = plot.get_figure()
     fig.savefig("task-d/f.jpg")
 
 def part_g(df):
     df = df.filter(df['Response Code'] >= 400).groupBy('Remote Host').count().toPandas().sort_values('count', ascending=False).head(5)
+    # df = df_cache.filter(df_cache['Response Code'] >= 400).toPandas().sort_values('count', ascending=False).head(5)
     with open("task-d/g.txt", "w") as f:
         f.write("Failed HTTP Clients:\n")
         f.write(df[['Remote Host']].to_string(index=False, header=False))
@@ -185,28 +184,29 @@ def part_j(df):
 
 
 if __name__ == "__main__":
-    # print(reg_split('66.249.66.194 - - [22/Jan/2019:03:56:20 +0330] "GET /m/filter/b2,p6 HTTP/1.1" 200 19451 "-" "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)" "-"'))
     spark = SparkSession \
     .builder \
     .appName("PySpark create RDD example") \
     .config("spark.some.config.option", "some-value") \
-    .getOrCreate()  
-
-    csv_file = './access.log'
+    .getOrCreate()
+    
+    csv_file = '/Users/vibhavaggarwal/Downloads/access.log'
     rdd_c = get_rdd(spark, csv_file)
 
     df = rdd_c.toDF()
-    # df.cache()
-    
+
+    df = df.drop('Request Method')
+    df.cache()
+   
     if not os.path.exists('task-d'):
         os.makedirs('task-d')
     
-    part_a(df)
-    part_b(df)
-    part_c(df)
-    part_d(df)
-    part_e(df)
-    part_f(df)
+    df_cache_a = part_a(df)
+    part_b(df_cache_a)
+    df_cache_c = part_c(df)
+    part_d(df_cache_c)
+    df_cache_e = part_e(df)
+    part_f(df_cache_e)
     part_g(df)
     part_h(df)
     part_i(df)
